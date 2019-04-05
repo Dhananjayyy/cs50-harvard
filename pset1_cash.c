480086dfb841ef6e3390e6bf77a2311737fe070c
Dhananjayyy <36818729+Dhananjayyy@users.noreply.github.com>
2019-04-05 10:45:00 +0530

    Add files via upload
    A program that first asks the user how much change is owed and then prints the minimum number of coins with which that change can be made.

---
+++ pset1_cash.c
@@ -0,0 +1,28 @@
+// Dhananjay Yelwande
+// April 5th, 2019
+// Problem Set 1 : Cash
+
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main()
+{
+    int cents;
+    do
+    {
+        // Accepts user input in decimals
+        float dollars = get_float("Owed change: ");
+        // Rounds cents to nearest penny
+        cents = round(dollars * 100);
+    }
+    while (cents <= 0);
+
+    // Conversion of user's inputted dollars
+    int quarters = cents / 25;
+    int dimes = (cents % 25) / 10;
+    int nickels = ((cents % 25) % 10) / 5;
+    int pennies = ((cents % 25) % 10) % 5;
+
+    printf("%d\n", quarters + dimes + nickels + pennies);
+}
